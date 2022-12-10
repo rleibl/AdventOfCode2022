4e005d32e5a1618934054adc3e1d29f151320183
ovid <robert.leibl@gmail.com>
2022-12-10 19:10:11 +0100

    Add day 06

---
+++ 06/aoc06_part1.py
@@ -0,0 +1,31 @@
+
+f = open('input')
+
+def unique(arr):
+
+    t = arr.copy()
+    t.sort()
+    for i in range(len(t)-1):
+        if t[i] == t[i+1]:
+            return False
+
+    return True
+
+count = 0
+window = ['q'] * 4
+
+for c in f.read():
+
+    if not c or c == "\n":
+        break
+
+    count += 1
+    window = window[1:]
+    window.append(c)
+
+    if unique(window):
+        print(window)
+        print(count)
+        break
+
+
